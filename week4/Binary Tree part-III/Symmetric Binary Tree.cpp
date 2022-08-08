265dbb15d527de3cf7547f5e378ebbcd17cee629
Ayushchauhan121 <93342982+Ayushchauhan121@users.noreply.github.com>
2022-08-09 02:57:55 +0530

    Create Symmetric Binary Tree.cpp

---
+++ week4/Binary Tree part-III/Symmetric Binary Tree.cpp
@@ -0,0 +1,36 @@
+class BinaryTreeNode {
+        public : 
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+        
+        ~BinaryTreeNode() {
+            if(left) 
+                delete left;
+            if(right) 
+                delete right;
+        }
+    };
+******************************************************/
+bool isIdentical(BinaryTreeNode<int>* root1, BinaryTreeNode<int>* root2) {
+    if(root1 == NULL || root2 == NULL) return root1==root2;
+    return ((root1->data == root2->data) && isIdentical(root1->left, root2->right) && isIdentical(root1->right, root2->left));
+}
+
+bool isSymmetric(BinaryTreeNode<int>* root)
+{
+    // Write your code here.  
+     if(!root) return true;
+    return isIdentical(root->left, root->right);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
